ad40e4c55bd21cbdcdbf8959199881868c5cf177
raphaelMi <raphael.m024@gmail.com>
2020-04-28 01:16:10 +0200

    cucker smale flocking implemented!

--- main.py
+++ main.py
@@ -15,22 +15,24 @@ fish_image = pg.image.load('fish.png')
 pg.display.set_icon(fish_image)
 pg.display.set_caption("Flocking simulator")
 
-fish = Flock(100)
-fish.positions = np.random.normal((screen_x/2, screen_y/2), 100, (fish.population, 2))
-fish.velocities = np.random.normal((0, 0), 2, (fish.population, 2))
+fish = Flock(50)
+fish.positions = np.random.normal((screen_x/2, screen_y/2), 50, (fish.population, 2))
+fish.velocities = np.random.normal((0, 0), 100, (fish.population, 2))
 fish.dimensions = (screen_x, screen_y)
 
 running = True
 clock = pg.time.Clock()
 while running:
-    clock.tick()
+    clock.tick(120)
     screen.fill((255, 255, 255,))
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-    fish.do_frame(clock.get_fps())
+    fish.do_frame(clock.get_time())
+    # print(clock.get_fps())
+
     for pos, vec in zip(fish.positions, fish.directions):
         pos1 = (pos + 10 * vec).astype(np.int32)
         pos2 = pos.astype(np.int32)
